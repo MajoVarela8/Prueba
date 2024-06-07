439d3f6dcd0564a9fd8bc535e6f1abd5b1d92d4c
varel <majovarela_8@hotmail.com>
2024-06-07 19:19:40 -0300

    adjunte boton, imagenes, nueva activity

--- app/src/main/java/com/juanrosasdev/toolandroid/MainActivity.kt
+++ app/src/main/java/com/juanrosasdev/toolandroid/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.juanrosasdev.toolandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn: Button=findViewById(R.id.button3)
+        btn.setOnClickListener {
+            val intent: Intent=Intent(this, pantalla2:: class.java)
+            startActivity(intent)
+        }
     }
 }
